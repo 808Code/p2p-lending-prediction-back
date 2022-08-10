4edd0d3bb4520678165ca2b38120864b45f09ef8
808Code <dikshant.shah2k@gmail.com>
2022-08-10 10:06:54 +0545

    form completed

--- predict/forms.py
+++ predict/forms.py
@@ -1,27 +1,27 @@
 from django import forms
 
+
 class PreditctForm(forms.Form):
-    loan_amount = forms.IntegerField(max_length=100)
-    term = forms.IntegerField(max_length = 100)
-    int_rate = forms.FloatField(max_length = 200)
-    installment = forms.FloatField(max_length = 200)
+    loan_amount = forms.IntegerField()
+    term = forms.IntegerField()
+    int_rate = forms.FloatField()
+    installment = forms.FloatField()
     emp_title = forms.CharField()
     home_ownership = forms.CharField()
-    annual_inc = forms.FloatField(max_length = 200)
+    annual_inc = forms.FloatField()
     verification_status = forms.BooleanField()
     purpose = forms.CharField()
-    dti =forms.FloatField(max_length=200)
-    #drop
-    pub_rec = forms.IntegerField(max_length = 100)
-    revol_bal = forms.FloatField(max_length = 200)
-    #drop
-    total_acc = forms.FloatField(max_length = 200)
-    initial_list_status	= forms.IntegerField(max_length = 100)
+    dti = forms.FloatField()
+    # drop
+    pub_rec = forms.IntegerField()
+    revol_bal = forms.FloatField()
+    # drop
+    total_acc = forms.FloatField()
+    initial_list_status = forms.IntegerField()
     application_type = forms.CharField()
-    pub_rec_bankruptcies = forms.IntegerField(max_length = 100)
+    pub_rec_bankruptcies = forms.IntegerField()
     address = forms.CharField()
     issue_d_month = forms.CharField()
     issue_d_year = forms.CharField()
     earliest_cr_line_month = forms.CharField()
     earliest_cr_line_year = forms.CharField()
-
--- predict/views.py
+++ predict/views.py
@@ -1,25 +1,14 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-
-from loanprediction.predict.forms import PreditctForm
-
+from .forms import PreditctForm
 
 def index(request):
     if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
         form = PreditctForm(request.POST)
-        # check whether it's valid:
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
-
-    # if a GET (or any other method) we'll create a blank form
+            print(form)
+            context = {'form': form}
+            return render(request, 'predict/index.html', context)
     else:
-        form = NameForm()
-
-    return render(request, 'name.html', {'form': form})
+        form = PreditctForm()
+        context = {'form': form}
     return render(request, 'predict/index.html', context)
