ea910b769b479625824261914d3781f6d12d8d06
808Code <dikshant.shah2k@gmail.com>
2022-08-10 16:55:40 +0545

    making a pickle file load

--- predict/views.py
+++ predict/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .forms import PreditctForm
+from loanprediction.loanprediction.settings import ML_MODEL
 
 def index(request):
+    print(ML_MODEL)
     if request.method == 'POST':
         form = PreditctForm(request.POST)
         if form.is_valid():
